502651908f13a433c35f1b35db7701f5094a0f3e
eliseven-7 <feng.yinu@northeastern.edu>
2025-03-14 23:56:51 -0700

    Encrypt password using md5

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -38,8 +38,10 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
         }
 
+        //MD5加密
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
